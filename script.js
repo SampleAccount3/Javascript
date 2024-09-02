d86b983f1145ca5ba77c0583b5cc093f2cc558d8
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-09-02 14:53:39 +0800

    Init Comit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Sample");
+
+document.title = "Dom Changed the Title";
+
+console.log(document.title);
+const thisWillUpdateTheH1 = "This is the Updated Hello World"
+
+const h1Message = document.getElementById("msg-HelloWorld");
+
+h1Message.textContent = thisWillUpdateTheH1;
